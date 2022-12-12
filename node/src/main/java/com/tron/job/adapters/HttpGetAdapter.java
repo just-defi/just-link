868a0756c74f207450dec66cfee08a1b57c15f44
chenzi <michael.chen@tron.network>
2022-12-12 11:17:21 +0800

    Log httpget raw request and response

--- node/src/main/java/com/tron/job/adapters/HttpGetAdapter.java
+++ node/src/main/java/com/tron/job/adapters/HttpGetAdapter.java
@@ -40,7 +40,6 @@ public class HttpGetAdapter extends BaseAdapter {
     String response = null;
     try {
       response = HttpUtil.requestWithRetry(url);
-      log.info("HttpGet Request: {} | Response: {}", url, response);
     } catch (IOException e) {
       log.error("Http Request failed, err:" + e.getMessage(), e);
     }
