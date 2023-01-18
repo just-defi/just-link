e4bce860de1daba47f7c9a3b0d5bd6261dd94fa0
soonweng.sit <soonweng.sit@tron.network>
2023-01-18 14:51:46 +0800

    Updated: [build.gradle]: changed compile to implementation [DetailActiveJob.java]: Updated class [JobSpec.java]: Updated class

--- node/src/main/java/com/tron/web/entity/DetailActiveJob.java
+++ node/src/main/java/com/tron/web/entity/DetailActiveJob.java
@@ -1,46 +1,31 @@
 package com.tron.web.entity;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
 import lombok.*;
 
-//@Data
-@NoArgsConstructor
+
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @Getter
 @Setter
 public class DetailActiveJob extends JobSpec implements Serializable {
-  private String id;
-  private Date createdAt;
-  private List<Initiator> initiators;
-  private List<TaskSpec> taskSpecs;
-  private Long minPayment;
-  private Date startAt;
-  private Date endAt;
-  private Date updatedAt;
-  private Date deletedAt;
-  private String params;
   private Long result;
 
-  @Override
-  public boolean archived() {
-    return deletedAt != null;
-  }
-
   public DetailActiveJob(JobSpec jobSpec, Long result) {
-    this.id = jobSpec.getId();
-    this.createdAt = jobSpec.getCreatedAt();
-    this.initiators = jobSpec.getInitiators();
-    this.taskSpecs = jobSpec.getTaskSpecs();
-    this.minPayment = jobSpec.getMinPayment();
-    this.startAt = jobSpec.getStartAt();
-    this.endAt = jobSpec.getEndAt();
-    this.updatedAt = jobSpec.getUpdatedAt();
-    this.deletedAt = jobSpec.getDeletedAt();
-    this.params = jobSpec.getParams();
+    super(
+      jobSpec.getId(),
+      jobSpec.getCreatedAt(),
+      jobSpec.getInitiators(),
+      jobSpec.getTaskSpecs(),
+      jobSpec.getMinPayment(),
+      jobSpec.getStartAt(),
+      jobSpec.getEndAt(),
+      jobSpec.getUpdatedAt(),
+      jobSpec.getDeletedAt(),
+      jobSpec.getParams()
+    );
+
     this.result = result;
   }
 }
